db350764625a768b188ce59e7743f79c97f85106
DFgit1 <fiorentino.dmf@gmail.com>
2022-11-08 20:46:13 -0500

    updated fixes

--- index.ts
+++ index.ts
@@ -6,10 +6,10 @@ class students {
     gender;
     nationality;
 
-    constructor(stud_name: string, stud_age: number, stud_gender: "Female", stud_nation: string) {
+    constructor(stud_name: string, stud_age: number, stud_nation: string) {
         this.name = stud_name;
         this.age = stud_age;
-        this.gender = stud_gender;
+        this.gender = "Female";
         this.nationality = stud_nation;
     }
     getNationality(){
@@ -17,14 +17,12 @@ class students {
     }  
 }
 
-// interface Istudents{
-//     name:string;
-//     age:number;
-//     gender:string;
-//     nationality:string;
-// }
 
 interface Iundergrad {
+    name: string;
+    age: number;
+    gender: "Female";
+    nationality: string;
     batch:string;
     type:number;
     gpa:number;
@@ -34,8 +32,8 @@ class undergrad extends students implements Iundergrad {
     type:number;
     gpa:number;
 
-    constructor(stud_name: string, stud_age: number, stud_gender: "Female", stud_nation: string, under_batch: string, stud_type:number, stud_gpa:number) {
-        super(stud_name, stud_age, stud_gender, stud_nation);
+    constructor(stud_name: string, stud_age: number, stud_nation: string, under_batch: string, stud_type:number, stud_gpa:number) {
+        super(stud_name, stud_age, stud_nation);
         this.batch = under_batch;
         this.type = stud_type;
         this.gpa = stud_gpa;
@@ -43,20 +41,9 @@ class undergrad extends students implements Iundergrad {
 }
 
 
-let student1 = new students("Wendy Brader", 54, "Female", "American");
-let student2 = new students("Mary Tavish", 26, "Female", "Spanish");
-
-
-
-// let manystudents:students[] = [
-//     {name:"Wendy Brader", age: 54, gender: "Female", nationality: "American"},
-//     {name: "Jane Weber", age: 15, gender: "Female", nationality: "Canadian"},
-//     {name: "Melanie Liberte", age: 42, gender: "Female", nationality: "French"},
-//     {name: "Melissa Luga", age:62, gender: "Female", nationality: "Polish"},
-//     {name: "Danielle Delmar", age: 22, gender: "Female", nationality: "Spanish"},
-// ]
-
-let undergrad1 = new undergrad("Victoria", 45, "Female", "Canadian", "Secondary", 5, 87);
+let student1 = new students("Wendy Brader", 54, "American");
+let student2 = new students("Mary Tavish", 26, "Spanish");
+let undergrad1 = new undergrad("Victoria", 45, "Female", "Canadian", 76, 87);
 
 
 console.log(student1.getNationality());
